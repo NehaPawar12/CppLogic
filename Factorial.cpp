0799da63d1a837a73fa73a0078e9bd1b6e6aaa84
Neha Pawar <nehap@pre-scient.com>
2026-06-05 10:14:52 +0530

    Added recursion to the code.

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
  
 int main()
 {
@@ -7,14 +14,16 @@ int main()
   cout<<"Enter a positive integer: ";
   cin>>n;
 
-  int factorial = 1;
+  int fact = 1;
 
   for(int i = 1; i <= n; ++i)
   {
-      factorial *= i;
+      fact *= i;
   }
 
-  cout << "Factorial of " << n << " = " << factorial << endl;
+  cout << "Factorial of " << n << " = " << fact<< endl;
+
+  cout << "Factorial of " << n << " (using recursion) = " << factorial(n) << endl;
 
   return 0;
 }
